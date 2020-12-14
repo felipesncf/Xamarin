8c2f73a06070119a5d959cd5c7aecde2df4e7353
felipesncf <piocarvalho_1@hotmail.com>
2020-12-14 11:11:04 -0300

    inicial do projeto

---
+++ ProjetoXamarin/ProjetoXamarin/ProjetoXamarin/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ProjetoXamarin.Models;
+
+namespace ProjetoXamarin.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
